0e7f118aebb03abfbbd316c5da5894fe335c0b8a
pontepath59 <PontePath@gmail.com>
2026-04-30 15:03:08 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+
+  return (
+
+    <div style={{ padding: 40 }}>
+
+      <h1>PontePath is Live 🙏</h1>
+
+      <p>Your app is working.</p>
+
+    </div>
+
+  );
+
+}
